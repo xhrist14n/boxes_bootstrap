0b7713664cf848ef5e6421d1a4df78c2634f0651
khrist14n <khrist14n@gmail.com>
2016-09-14 11:08:41 -0500

    project video player libraries

---
+++ git.sh
@@ -0,0 +1,20 @@
+clear
+git add ./bower_components/. 
+git add ./test
+git add ./app
+git add ./app/scripts/.
+git add ./app/styles/.
+git add ./app/images/.
+git add ./app/fonts/.
+git add bower.json
+git add git.sh
+git add Gruntfile.js
+git add package.json
+git add .bowerrc
+git add  .editorconfig
+git add  .gitattributes
+git add  .gitignore
+git add .yo-rc.json
+git add run.sh
+git commit -am "project video player libraries"
+git push origin master
